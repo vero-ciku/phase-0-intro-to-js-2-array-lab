85a8a60f6aa7288d1e3b170338020cb1ccc3183e
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-08 15:01:10 -0700

    commit tree 607859afcc836433626037c98b53fc3bf18b8935

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+    cats.push ('Milo', 'Otis', 'Garfield');
+});
+
+function destructivelyAppendCat() {
+    return cats.push('Ralph')
+ }
+ 
+ function destructivelyPrependCat() { 
+     return cats.unshift("Bob")
+ }
+
+ function destructivelyRemoveLastCat() {
+     return cats.pop()
+ }
+
+ function destructivelyRemoveFirstCat() {
+     return cats.shift()
+ }
+
+ function appendCat() {
+    return  [ ...cats, `Broom`]
+ }
+
+ function prependCat() {
+    return [ "Arnold",...cats]
+ }
+
+ function removeLastCat() {
+     return cats.slice(0, -1);
+     }
+
+     function removeFirstCat() {
+         return cats.slice(1, cats.length)
+     }
+
+
